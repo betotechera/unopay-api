80a6eee65382e886e71cd03752e8eea89ad6bd52
emerson <e.mauriciofreitas@gmail.com>
2017-02-20 12:12:19 -0300

    big bang

---
+++ src/main/java/br/com/unopay/api/UnopayApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.unopay.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnopayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnopayApiApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/unopay/api/UnopayApiApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.unopay.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UnopayApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
