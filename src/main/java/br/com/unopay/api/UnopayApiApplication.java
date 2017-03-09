deca7cae173f13b0405f0086a0c01397b8a218eb
Rodolfo Yanke <rodolfo.yanke@gmail.com>
2017-03-09 02:51:51 -0300

    adding stopwatch support

--- src/main/java/br/com/unopay/api/UnopayApiApplication.java
+++ src/main/java/br/com/unopay/api/UnopayApiApplication.java
@@ -1,10 +1,11 @@
 package br.com.unopay.api;
 
+import br.com.unopay.bootcommons.CommonsAutoConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
-@SpringBootApplication(scanBasePackageClasses = UnopayApiApplication.class)
+@SpringBootApplication(scanBasePackageClasses = {UnopayApiApplication.class, CommonsAutoConfig.class})
 @EnableGlobalMethodSecurity(securedEnabled = true, proxyTargetClass = true, prePostEnabled = true)
 public class UnopayApiApplication {
 
