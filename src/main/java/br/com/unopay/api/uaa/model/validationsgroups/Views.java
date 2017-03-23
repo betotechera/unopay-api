61be7d69bd8b3b710a623208f53fdb581974fc43
Diogo Techera <diogotechera@gmail.com>
2017-03-23 00:28:30 -0300

    getGroups by userType

--- src/main/java/br/com/unopay/api/uaa/model/validationsgroups/Views.java
+++ src/main/java/br/com/unopay/api/uaa/model/validationsgroups/Views.java
@@ -6,4 +6,6 @@ public interface Views {
     interface Internal {}
 
     interface List {}
+
+    interface GroupUserType {}
 }
--- src/main/java/br/com/unopay/api/uaa/service/UserTypeService.java
+++ src/main/java/br/com/unopay/api/uaa/service/UserTypeService.java
@@ -1,19 +1,31 @@
 package br.com.unopay.api.uaa.service;
 
+import br.com.unopay.api.uaa.model.Group;
 import br.com.unopay.api.uaa.model.UserType;
+import br.com.unopay.api.uaa.repository.GroupRepository;
 import br.com.unopay.api.uaa.repository.UserTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class UserTypeService {
 
     @Autowired
     private UserTypeRepository repository;
+    @Autowired
+    private GroupRepository groupRepository;
+
+    @Autowired
+    private GroupService groupService;
 
     public List<UserType> findAll(){
         return repository.findAll();
+    }
+
+    public List<Group> findUserTypeGroups(String userTypeId) {
+        return groupRepository.findByUserTypeId(userTypeId);
     }
 }
