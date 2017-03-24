61a8b28232063fdf5ce72c3fc25e64f4895dcbe5
emerson <emerson@unovation.com.br>
2017-03-24 18:27:48 -0300

    Committing release jar version 0.0.70

--- src/main/resources/dev/db/migration/hsqldb/V1_3__PaymentRuleGroup.sql
+++ src/main/resources/dev/db/migration/hsqldb/V1_3__PaymentRuleGroup.sql
@@ -12,6 +12,6 @@ create unique index payment_rule_group_uk on payment_rule_group(code);
 Insert into AUTHORITY(name, description) values('ROLE_LIST_PAYMENT_RULE_GROUP','Permite listar Arranjos de Pagamento');
 Insert into AUTHORITY(name, description) values('ROLE_MANAGE_PAYMENT_RULE_GROUP','Permite gerenciar Arranjos de Pagamento');
 
---ALTER TABLE oauth_user_details ADD payment_rule_group_id VARCHAR(256);
---ALTER TABLE oauth_user_details ADD CONSTRAINT FOREIGN KEY(payment_rule_group_id) REFERENCES payment_rule_group(id);
+ALTER TABLE oauth_user_details ADD payment_rule_group_id VARCHAR(256);
+ALTER TABLE oauth_user_details ADD CONSTRAINT fk_user_details_rule_group FOREIGN KEY(payment_rule_group_id) REFERENCES payment_rule_group(id);
 
