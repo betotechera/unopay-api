98d4334974dc53bd3728f5a1c9bd298ab336a7aa
Diogo Techera <diogotechera@gmail.com>
2017-03-24 22:34:04 -0300

    Committing release jar version 0.0.73

--- src/main/resources/db/migration/oracle/V1_3__PaymentRuleGroup.sql
+++ src/main/resources/db/migration/oracle/V1_3__PaymentRuleGroup.sql
@@ -13,7 +13,7 @@ create table payment_rule_group (
 Insert into AUTHORITY(name, description) values('ROLE_LIST_PAYMENT_RULE_GROUP','Permite listar Arranjos de Pagamento');
 Insert into AUTHORITY(name, description) values('ROLE_MANAGE_PAYMENT_RULE_GROUP','Permite gerenciar Arranjos de Pagamento');
 
-insert into PAYMENT_RULE_GROUP(id,name,code,purpose,scope,USER_RELATIONSHIP) values('66eac89e-10f8-11e7-93ae-92361f002671','Unovation','1234','BUY','DOMESTIC','PREPAID')
+insert into PAYMENT_RULE_GROUP(id,name,code,purpose,scope,USER_RELATIONSHIP) values('66eac89e-10f8-11e7-93ae-92361f002671','Unovation','1234','BUY','DOMESTIC','PREPAID');
 
 ALTER TABLE oauth_user_details ADD payment_rule_group_id VARCHAR(256);
 ALTER TABLE oauth_user_details ADD CONSTRAINT fk_user_details_rule_group FOREIGN KEY(payment_rule_group_id) REFERENCES payment_rule_group(id);
