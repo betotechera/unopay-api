58c9e577d973620331d9440eab8fe97e5b2fcc46
emerson <emerson@unovation.com.br>
2017-03-30 20:46:14 -0300

    add: filter fields

--- src/main/java/br/com/unopay/api/bacen/model/IssuerFilter.java
+++ src/main/java/br/com/unopay/api/bacen/model/IssuerFilter.java
@@ -6,6 +6,18 @@ import lombok.Data;
 @Data
 public class IssuerFilter {
 
-    @SearchableField
-    private String tax;
+    @SearchableField(field = "person.document.number")
+    private String documentNumber;
+
+    @SearchableField(field = "person.legalPersonDetail.fantasyName")
+    private String fantasyName;
+
+    @SearchableField(field = "person.address.streetName")
+    private String streetName;
+
+    @SearchableField(field = "person.address.city")
+    private String city;
+
+    @SearchableField(field = "person.address.state")
+    private String state;
 }
