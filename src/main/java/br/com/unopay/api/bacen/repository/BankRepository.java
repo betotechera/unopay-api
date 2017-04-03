7756554edccb73e94eba6a14613808b4626cbad8
Diogo Techera <diogotechera@gmail.com>
2017-04-03 17:50:24 -0300

    retrieve all banks for menu creation

--- src/main/java/br/com/unopay/api/bacen/repository/BankRepository.java
+++ src/main/java/br/com/unopay/api/bacen/repository/BankRepository.java
@@ -5,7 +5,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface BankRepository extends CrudRepository<Bank,Integer> {
 
     Page<Bank> findAll(Pageable pageable);
+    List<Bank> findAll();
 }
