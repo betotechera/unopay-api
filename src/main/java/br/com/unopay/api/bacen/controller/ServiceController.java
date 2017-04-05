59af768c7661deb42b05f086ec464204e3043c33
Diogo Techera <diogotechera@gmail.com>
2017-04-05 16:37:44 -0300

    Committing release jar version 0.0.110

--- src/main/java/br/com/unopay/api/bacen/controller/ServiceController.java
+++ src/main/java/br/com/unopay/api/bacen/controller/ServiceController.java
@@ -1,15 +1,20 @@
 package br.com.unopay.api.bacen.controller;
 
 import br.com.unopay.api.bacen.model.Service;
+import br.com.unopay.api.bacen.model.ServiceFilter;
 import br.com.unopay.api.bacen.service.ServiceService;
 import br.com.unopay.api.uaa.model.validationsgroups.Create;
 import br.com.unopay.api.uaa.model.validationsgroups.Update;
 import br.com.unopay.api.uaa.model.validationsgroups.Views;
+import br.com.unopay.bootcommons.jsoncollections.PageableResults;
+import br.com.unopay.bootcommons.jsoncollections.Results;
+import br.com.unopay.bootcommons.jsoncollections.UnovationPageRequest;
 import br.com.unopay.bootcommons.stopwatch.annotation.Timed;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -59,6 +64,16 @@ public class ServiceController {
     public void remove(@PathVariable  String id) {
         log.info("removing bank account id={}", id);
         service.delete(id);
+    }
+
+    @JsonView(Views.List.class)
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/services", method = RequestMethod.GET)
+    public Results<Service> getByParams(ServiceFilter filter, @Validated UnovationPageRequest pageable) {
+        log.info("search Service by filter with filter={}", filter);
+        Page<Service> page =  service.findByFilter(filter, pageable);
+        pageable.setTotal(page.getTotalElements());
+        return PageableResults.create(pageable, page.getContent(), String.format("%s/services", api));
     }
 
 }
--- src/main/java/br/com/unopay/api/bacen/service/ServiceService.java
+++ src/main/java/br/com/unopay/api/bacen/service/ServiceService.java
@@ -1,9 +1,13 @@
 package br.com.unopay.api.bacen.service;
 
 import br.com.unopay.api.bacen.model.Service;
+import br.com.unopay.api.bacen.model.ServiceFilter;
 import br.com.unopay.api.bacen.repository.ServiceRepository;
 import br.com.unopay.bootcommons.exception.UnovationExceptions;
+import br.com.unopay.bootcommons.jsoncollections.UnovationPageRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 import static br.com.unopay.api.uaa.exception.Errors.SERVICE_NOT_FOUND;
 
@@ -33,5 +37,9 @@ public class ServiceService {
     public void delete(String id) {
         findById(id);
         repository.delete(id);
+    }
+
+    public Page<Service> findByFilter(ServiceFilter filter, UnovationPageRequest pageable) {
+        return repository.findAll(filter, new PageRequest(pageable.getPageStartingAtZero(), pageable.getSize()));
     }
 }
