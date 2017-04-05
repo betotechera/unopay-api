ff4590e1fddcc886320b8bf6de20ce1069ac3561
emerson <emerson@unovation.com.br>
2017-04-05 13:40:25 -0300

    add: script role

--- src/main/resources/db/migration/oracle/V2_4__Service_Event.sql
+++ src/main/resources/db/migration/oracle/V2_4__Service_Event.sql
@@ -15,3 +15,17 @@ create table event (
  quantity_unity varchar(256),
  constraint fk_service_event foreign key(service_id) REFERENCES service(id)
 );
+
+
+insert into AUTHORITY(name, description) values('ROLE_LIST_EVENT','Permite listar Eventos');
+insert into AUTHORITY(name, description) values('ROLE_MANAGE_EVENT','Permite gerenciar Eventos');
+
+
+insert into oauth_group_authorities(authority, group_id) values('ROLE_LIST_EVENT', '99bf9ba6-75e4-4109-b5be-e4858f3f68b2');
+insert into oauth_group_authorities(authority, group_id) values('ROLE_MANAGE_EVENT', '99bf9ba6-75e4-4109-b5be-e4858f3f68b2');
+
+insert into AUTHORITY(name, description) values('ROLE_LIST_SERVICE','Permite listar Serviços');
+insert into AUTHORITY(name, description) values('ROLE_MANAGE_SERVICE','Permite gerenciar Serviços');
+
+insert into oauth_group_authorities(authority, group_id) values('ROLE_LIST_SERVICE', '99bf9ba6-75e4-4109-b5be-e4858f3f68b2');
+insert into oauth_group_authorities(authority, group_id) values('ROLE_MANAGE_SERVICE', '99bf9ba6-75e4-4109-b5be-e4858f3f68b2');
