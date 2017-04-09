c8e970c56b40e6574d5a4ee02f0e31eed55a63ee
emerson <emerson@unovation.com.br>
2017-04-09 19:07:21 -0300

    remove: import

--- src/main/java/br/com/unopay/api/bacen/service/SubsidiaryService.java
+++ src/main/java/br/com/unopay/api/bacen/service/SubsidiaryService.java
@@ -3,13 +3,10 @@ package br.com.unopay.api.bacen.service;
 import br.com.unopay.api.bacen.model.Subsidiary;
 import br.com.unopay.api.bacen.repository.SubsidiaryRepository;
 import br.com.unopay.api.service.PersonService;
-import br.com.unopay.bootcommons.exception.UnovationExceptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
-import static br.com.unopay.api.uaa.exception.Errors.SUBSIDIARY_NOT_FOUND;
 
 @Service
 public class SubsidiaryService {
