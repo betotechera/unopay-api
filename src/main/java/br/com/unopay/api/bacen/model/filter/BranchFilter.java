f6273ed7b5967e5d2193f56b0b06953c0068b826
emerson <emerson@unovation.com.br>
2017-04-09 21:43:30 -0300

    change: branch and establishment filters

--- src/main/java/br/com/unopay/api/bacen/model/filter/BranchFilter.java
+++ src/main/java/br/com/unopay/api/bacen/model/filter/BranchFilter.java
@@ -1,4 +1,25 @@
 package br.com.unopay.api.bacen.model.filter;
 
-public class BranchFilter {
+import br.com.unopay.api.repository.filter.SearchableField;
+
+import java.io.Serializable;
+
+public class BranchFilter implements Serializable {
+
+    public static final Long serialVersionUID = 1L;
+
+    @SearchableField(field = "person.document.number")
+    private String documentNumber;
+
+    @SearchableField(field = "person.name")
+    private String personName;
+
+    @SearchableField(field = "headOffice.type")
+    private String headOfficeType;
+
+    @SearchableField(field = "person.address.city")
+    private String city;
+
+    @SearchableField(field = "person.address.state")
+    private String state;
 }
--- src/main/java/br/com/unopay/api/bacen/model/filter/EstablishmentFilter.java
+++ src/main/java/br/com/unopay/api/bacen/model/filter/EstablishmentFilter.java
@@ -1,4 +1,25 @@
 package br.com.unopay.api.bacen.model.filter;
 
-public class EstablishmentFilter {
+import br.com.unopay.api.repository.filter.SearchableField;
+
+import java.io.Serializable;
+
+public class EstablishmentFilter implements Serializable {
+
+    public static final Long serialVersionUID = 1L;
+
+    @SearchableField(field = "person.document.number")
+    private String documentNumber;
+
+    @SearchableField(field = "person.name")
+    private String personName;
+
+    @SearchableField
+    private String type;
+
+    @SearchableField(field = "person.address.city")
+    private String city;
+
+    @SearchableField(field = "person.address.state")
+    private String state;
 }
