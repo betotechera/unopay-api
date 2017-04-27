3e147691ac14596424a93445506b652dd8ccd1c4
Diogo Techera <diogotechera@gmail.com>
2017-04-27 01:58:27 -0300

    CONTRACT FILTER

--- src/main/java/br/com/unopay/api/model/filter/ContractFilter.java
+++ src/main/java/br/com/unopay/api/model/filter/ContractFilter.java
@@ -1,4 +1,29 @@
 package br.com.unopay.api.model.filter;
 
-public class ContractFilter {
+import br.com.unopay.api.model.ContractSituation;
+import br.com.unopay.api.repository.filter.SearchableField;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class ContractFilter  implements Serializable {
+
+    public static final Long serialVersionUID = 1L;
+
+    @SearchableField
+    private String code;
+
+    @SearchableField(field = "product.id")
+    private String product;
+
+    @SearchableField(field = "hirer.id")
+    private String hirer;
+
+    @SearchableField(field = "contractor.id")
+    private String contractor;
+
+    @SearchableField
+    private ContractSituation situation;
+    
+
 }
