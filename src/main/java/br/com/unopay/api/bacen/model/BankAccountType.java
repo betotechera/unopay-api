44096268bc810c9d66c50523ef8af56521686dbb
Diogo Techera <diogotechera@gmail.com>
2017-04-28 18:25:28 -0300

    bankAccountType

--- src/main/java/br/com/unopay/api/bacen/model/BankAccountType.java
+++ src/main/java/br/com/unopay/api/bacen/model/BankAccountType.java
@@ -1,15 +1,5 @@
 package br.com.unopay.api.bacen.model;
 
-import br.com.unopay.api.filter.DescriptionEnum;
-
-public enum BankAccountType implements DescriptionEnum {
-    CURRENT("Corrente"), SAVINGS("Poupança");
-    private String description;
-
-    BankAccountType(String description){
-        this.description = description;
-    }
-    public String getDescription() {
-        return description;
-    }
+public enum BankAccountType  {
+    CURRENT, SAVINGS
 }
