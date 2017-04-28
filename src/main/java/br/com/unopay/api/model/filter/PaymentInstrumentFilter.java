a8e26bb5fca0a1c49346d7ee04b2ab0ab3b4aa73
Diogo Techera <diogotechera@gmail.com>
2017-04-28 01:32:44 -0300

    Committing release jar version 0.0.155

--- src/main/java/br/com/unopay/api/model/filter/PaymentInstrumentFilter.java
+++ src/main/java/br/com/unopay/api/model/filter/PaymentInstrumentFilter.java
@@ -1,4 +1,26 @@
 package br.com.unopay.api.model.filter;
 
-public class PaymentInstrumentFilter {
+import br.com.unopay.api.model.ContractSituation;
+import br.com.unopay.api.model.PaymentInstrumentSituation;
+import br.com.unopay.api.repository.filter.SearchableField;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class PaymentInstrumentFilter implements Serializable{
+
+    @SearchableField
+    private String number;
+
+    @SearchableField(field = "product.code")
+    private String productCode;
+
+    @SearchableField(field = "hirer.id")
+    private String hirer;
+
+    @SearchableField
+    private PaymentInstrumentSituation situation;
+
+
 }
