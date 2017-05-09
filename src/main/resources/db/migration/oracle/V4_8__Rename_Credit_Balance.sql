e3048dd340dfdd56b6de17b8b759d9f239835c7d
emerson <emerson@unovation.com.br>
2017-05-09 17:15:08 -0300

    fix rename

--- src/main/resources/db/migration/oracle/V4_8__Rename_Credit_Balance.sql
+++ src/main/resources/db/migration/oracle/V4_8__Rename_Credit_Balance.sql
@@ -1,2 +1,2 @@
-alter table credit rename available_balance to available_value;
-alter table credit rename blocked_balance to blocked_value;
+alter table credit rename column available_balance to available_value;
+alter table credit rename column blocked_balance to blocked_value;
