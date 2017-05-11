2225be2e9e01caa304b96f732cb3664ce9fa3b92
emerson <emerson@unovation.com.br>
2017-05-11 12:01:00 -0300

    add: contractor insturment credit model

---
+++ src/main/java/br/com/unopay/api/model/ContractorInstrumentCredit.java
@@ -0,0 +1,109 @@
+package br.com.unopay.api.model;
+
+import br.com.unopay.api.bacen.model.ServiceType;
+import br.com.unopay.api.uaa.model.validationsgroups.Create;
+import br.com.unopay.api.uaa.model.validationsgroups.Update;
+import br.com.unopay.api.uaa.model.validationsgroups.Views;
+import com.fasterxml.jackson.annotation.JsonView;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+
+@Data
+@Entity
+@EqualsAndHashCode(exclude = {"paymentInstrument", "contract"})
+@ToString(exclude = {"paymentInstrument", "contract"})
+@Table(name = "contractor_instrument_credit")
+public class ContractorInstrumentCredit implements Serializable, Updatable {
+
+    public ContractorInstrumentCredit(){}
+
+    public static final long serialVersionUID = 1L;
+
+
+    @Id
+    @Column(name="id")
+    @JsonView({Views.Public.class,Views.List.class})
+    @GenericGenerator(name="system-uuid", strategy="uuid2")
+    @GeneratedValue(generator="system-uuid")
+    private String id;
+
+    @ManyToOne
+    @JoinColumn(name="payment_instrument_id")
+    @NotNull(groups = {Create.class, Update.class})
+    @JsonView({Views.Public.class,Views.List.class})
+    private PaymentInstrument paymentInstrument;
+
+    @ManyToOne
+    @JoinColumn(name="contract_id")
+    @NotNull(groups = {Create.class, Update.class})
+    @JsonView({Views.Public.class,Views.List.class})
+    private Contract contract;
+
+    @Column(name = "service_type")
+    @Enumerated(EnumType.STRING)
+    @JsonView({Views.Public.class,Views.List.class})
+    private ServiceType serviceType;
+
+    @Column(name = "credit_insertion_type")
+    @Enumerated(EnumType.STRING)
+    @JsonView({Views.Public.class,Views.List.class})
+    @NotNull(groups = {Create.class, Update.class})
+    private CreditInsertionType creditInsertionType;
+
+    @Column(name = "installment_number")
+    @JsonView({Views.Public.class,Views.List.class})
+    @NotNull(groups = {Create.class, Update.class})
+    private Long installmentNumber;
+
+    @Column(name = "value")
+    @JsonView({Views.Public.class,Views.List.class})
+    @NotNull(groups = {Create.class, Update.class})
+    private BigDecimal value;
+
+    @Column(name = "expiration_date_time")
+    @JsonView({Views.Public.class,Views.List.class})
+    @NotNull(groups = {Create.class, Update.class})
+    private Date expirationDateTime;
+
+    @Column(name = "issuer_fee")
+    @JsonView({Views.Public.class,Views.List.class})
+    @NotNull(groups = {Create.class, Update.class})
+    private BigDecimal issuerFee;
+
+    @Column(name = "situation")
+    @JsonView({Views.Public.class,Views.List.class})
+    @NotNull(groups = {Create.class, Update.class})
+    private CreditSituation situation;
+
+    @Column(name = "available_balance")
+    @JsonView({Views.Public.class,Views.List.class})
+    @NotNull(groups = {Create.class, Update.class})
+    private BigDecimal availableBalance;
+
+    @Column(name = "blocked_balance")
+    @JsonView({Views.Public.class,Views.List.class})
+    @NotNull(groups = {Create.class, Update.class})
+    private BigDecimal blockedBalance;
+
+    @Column(name = "created_date_time")
+    @JsonView({Views.Public.class,Views.List.class})
+    @NotNull(groups = {Create.class, Update.class})
+    private Date createdDateTime;
+}
