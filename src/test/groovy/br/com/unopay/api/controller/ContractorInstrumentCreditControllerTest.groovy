3c84d721b65c39592e4da3a1fa6f25d0da6a385d
emerson <emerson@unovation.com.br>
2017-05-13 23:46:38 -0300

    add: test of cancel instrument credit

--- src/test/groovy/br/com/unopay/api/controller/ContractorInstrumentCreditControllerTest.groovy
+++ src/test/groovy/br/com/unopay/api/controller/ContractorInstrumentCreditControllerTest.groovy
@@ -3,10 +3,12 @@ package br.com.unopay.api.controller
 import br.com.unopay.api.bacen.util.SetupCreator
 import br.com.unopay.api.model.ContractorInstrumentCredit
 import br.com.unopay.api.model.Credit
+import br.com.unopay.api.service.ContractorInstrumentCreditService
 import br.com.unopay.api.uaa.AuthServerApplicationTests
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
@@ -14,6 +16,9 @@ class ContractorInstrumentCreditControllerTest extends AuthServerApplicationTest
 
     @Autowired
     SetupCreator setupCreator
+
+    @Autowired
+    ContractorInstrumentCreditService service
 
     void 'valid payment instrument credit should be created'() {
         given:
@@ -30,5 +35,25 @@ class ContractorInstrumentCreditControllerTest extends AuthServerApplicationTest
                 .content(toJsonWithoutNetworkPaymentRuleGroups(credit)))
         then:
         result.andExpect(status().isCreated())
+    }
+
+
+    void 'valid payment instrument credit should be canceled'() {
+        given:
+        String accessToken = getUserAccessToken()
+
+        ContractorInstrumentCredit credit = setupCreator.createContractorInstrumentCredit()
+
+        String instrumentId = credit?.paymentInstrument?.id
+        def inserted = service.insert(instrumentId, credit)
+        def id = inserted.id
+        when:
+        def result = this.mvc.perform(
+                delete('/payment-instruments/{instrumentId}/credits/{id}?access_token={access_token}',
+                        instrumentId, id, accessToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(toJsonWithoutNetworkPaymentRuleGroups(credit)))
+        then:
+        result.andExpect(status().isNoContent())
     }
 }
