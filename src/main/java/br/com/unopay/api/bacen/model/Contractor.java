7a104bdbf842665204d0691086cbb3f6825874f1
emerson <emerson@unovation.com.br>
2017-05-19 20:09:44 -0300

    organize imports

--- src/main/java/br/com/unopay/api/bacen/model/Contractor.java
+++ src/main/java/br/com/unopay/api/bacen/model/Contractor.java
@@ -10,7 +10,14 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
