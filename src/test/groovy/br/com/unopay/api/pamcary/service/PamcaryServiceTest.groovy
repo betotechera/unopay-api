694cbc18c40e9e97804d7067ff7dbee4334d4d17
emerson <emerson@unovation.com.br>
2017-06-05 18:35:17 -0300

    fix test

--- src/test/groovy/br/com/unopay/api/pamcary/service/PamcaryServiceTest.groovy
+++ src/test/groovy/br/com/unopay/api/pamcary/service/PamcaryServiceTest.groovy
@@ -1,14 +1,7 @@
 package br.com.unopay.api.pamcary.service
 
 import br.com.unopay.api.SpockApplicationTests
-import br.com.unopay.api.pamcary.service.PamcaryService
-import com.sun.xml.internal.ws.developer.JAXWSProperties
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory
-import org.jboss.ws.core.StubExt
 import org.springframework.beans.factory.annotation.Autowired
-
-import javax.net.ssl.SSLSocketFactory
-import javax.xml.ws.BindingProvider
 
 class PamcaryServiceTest extends SpockApplicationTests{
 
