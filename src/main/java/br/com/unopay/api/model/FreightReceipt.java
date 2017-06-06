1eebccd4ffc7b2271565d15d0d8c7de550e4321f
emerson <emerson@unovation.com.br>
2017-06-06 19:07:55 -0300

    add: model annotations

--- src/main/java/br/com/unopay/api/model/FreightReceipt.java
+++ src/main/java/br/com/unopay/api/model/FreightReceipt.java
@@ -3,8 +3,14 @@ package br.com.unopay.api.model;
 import br.com.unopay.api.bacen.model.Contractor;
 import br.com.unopay.api.bacen.model.Establishment;
 import br.com.unopay.api.bacen.model.ServiceType;
+import br.com.unopay.api.uaa.model.validationsgroups.Create;
+import br.com.unopay.api.uaa.model.validationsgroups.Update;
+import br.com.unopay.api.uaa.model.validationsgroups.Views;
+import com.fasterxml.jackson.annotation.JsonView;
 import java.io.Serializable;
 import java.util.List;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -14,10 +20,29 @@ public class FreightReceipt implements Serializable {
 
     public FreightReceipt(){}
 
+    @NotNull(groups = {Create.class,Update.class})
+    @JsonView({Views.Public.class})
     private Contract contract;
+
+    @NotNull(groups = {Create.class,Update.class})
+    @JsonView({Views.Public.class})
     private Contractor contractor;
+
+    @NotNull(groups = {Create.class,Update.class})
+    @JsonView({Views.Public.class})
     private Establishment establishment;
+
+    @NotNull(groups = {Create.class,Update.class})
+    @JsonView({Views.Public.class})
     private ServiceType serviceType;
+
+    @Valid
+    @NotNull(groups = {Create.class,Update.class})
+    @JsonView({Views.Public.class})
     private List<TravelDocument> travelDocuments;
+
+    @Valid
+    @NotNull(groups = {Create.class,Update.class})
+    @JsonView({Views.Public.class})
     private CargoContract cargoContract;
 }
