eb81cc27cfd5907cef34e06fe8538385ae898478
emerson <emerson@unovation.com.br>
2017-06-14 17:23:00 -0300

    Committing release jar version 0.0.227

---
+++ src/main/java/br/com/unopay/api/pamcary/receiver/PamcaryReceiver.java
@@ -0,0 +1,49 @@
+package br.com.unopay.api.pamcary.receiver;
+
+import br.com.unopay.api.config.Queues;
+import br.com.unopay.api.model.CargoContract;
+import br.com.unopay.api.model.FreightReceipt;
+import br.com.unopay.api.notification.model.Notification;
+import br.com.unopay.api.pamcary.service.PamcaryService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+class PamcaryReceiver {
+
+    private ObjectMapper objectMapper;
+    private PamcaryService pamcaryService;
+
+    @Autowired
+    public PamcaryReceiver(ObjectMapper objectMapper, PamcaryService pamcaryService) {
+        this.objectMapper = objectMapper;
+        this.pamcaryService = pamcaryService;
+    }
+
+    @RabbitListener(queues = Queues.PAMCARY_TRAVEL_DOCUMENTS)
+    void freightReceiptNotify(String notificationAsString) {
+        FreightReceipt freightReceipt = getAsObject(notificationAsString, FreightReceipt.class);
+        if(freightReceipt.getEstablishment() != null) {
+            String documentNumber = freightReceipt.getEstablishment().documentNumber();
+            log.info("confirmDocDelivery received to establishment={}", documentNumber);
+            pamcaryService.confirmDocDelivery(documentNumber, freightReceipt.getCargoContract());
+            pamcaryService.updateDoc(documentNumber, freightReceipt.getCargoContract());
+            return;
+        }
+        log.error("invalid freightReceipt received when try confirmDocDelivery");
+    }
+
+    <T> T getAsObject(String notificationAsString, Class<T> klass) {
+        try {
+            return objectMapper.readValue(notificationAsString,klass);
+        } catch (IOException e) {
+            log.error("unable to parse class={}",klass.getSimpleName(), e);
+            return null;
+        }
+    }
+}
--- src/test/groovy/br/com/unopay/api/pamcary/service/PamcaryServiceIntegrationTest.groovy
+++ src/test/groovy/br/com/unopay/api/pamcary/service/PamcaryServiceIntegrationTest.groovy
@@ -2,7 +2,6 @@ package br.com.unopay.api.pamcary.service
 
 import br.com.six2six.fixturefactory.Fixture
 import br.com.six2six.fixturefactory.Rule
-import br.com.unopay.api.SpockApplicationTests
 import br.com.unopay.api.model.CargoContract
 import br.com.unopay.api.model.ComplementaryTravelDocument
 import br.com.unopay.api.model.TravelDocument
