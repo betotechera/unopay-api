4f4ce84df20125ed311d8d79418c10e246116b7f
emerson <emerson@unovation.com.br>
2017-06-17 16:31:42 -0300

    fix script

--- src/main/resources/db/migration/oracle/V6_1__Credit_Fix.sql
+++ src/main/resources/db/migration/oracle/V6_1__Credit_Fix.sql
@@ -1,6 +1,6 @@
 alter table contractor_instrument_credit add credit_type varchar(100) default 'PAY_ADVANCE' not null;
-alter table issuer rename column fee to fee;
-alter table branch rename column fee to fee;
-alter table establishment rename column fee to fee;
+alter table issuer rename column tax to fee;
+alter table branch rename column tax to fee;
+alter table establishment rename column tax to fee;
 alter table service rename column tax_val to fee_val;
 alter table service rename column tax_percent to fee_percent;
