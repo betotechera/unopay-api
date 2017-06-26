6c9cc667cfc4c1991bb6f436d83770c575fc91d9
emerson <emerson@unovation.com.br>
2017-06-26 00:10:15 -0300

    fix test

--- src/test/groovy/br/com/unopay/api/bacen/util/SetupCreator.groovy
+++ src/test/groovy/br/com/unopay/api/bacen/util/SetupCreator.groovy
@@ -353,6 +353,7 @@ class SetupCreator {
             contractor = instrumentCreditUnderTest.contract.contractor
             event = createEvent(ServiceType.FUEL_ALLOWANCE)
             serviceType = ServiceType.FUEL_ALLOWANCE
+            eventValue = instrumentCreditUnderTest.availableBalance - 0.1
             user = createUser()
             contractorInstrumentCredit = instrumentCreditUnderTest
             establishment = establishmentUnderTest
