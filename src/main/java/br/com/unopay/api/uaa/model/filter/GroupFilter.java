189894f7a39e908166f72eb151d3b8fc9ddb9ab7
Diogo Techera <diogotechera@gmail.com>
2017-06-27 15:22:22 -0300

    groupFIlter data

--- src/main/java/br/com/unopay/api/uaa/model/filter/GroupFilter.java
+++ src/main/java/br/com/unopay/api/uaa/model/filter/GroupFilter.java
@@ -1,7 +1,11 @@
 package br.com.unopay.api.uaa.model.filter;
 
 import br.com.unopay.api.repository.filter.SearchableField;
+import lombok.Data;
+import lombok.ToString;
 
+@Data
+@ToString
 public class GroupFilter {
 
     @SearchableField(field = "userType.id")
