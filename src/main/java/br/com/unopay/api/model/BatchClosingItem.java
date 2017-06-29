c0b3abab5ac78bc37855fc700926ce41ca58cee9
emerson <emerson@unovation.com.br>
2017-06-29 17:03:40 -0300

    remove import

--- src/main/java/br/com/unopay/api/model/BatchClosingItem.java
+++ src/main/java/br/com/unopay/api/model/BatchClosingItem.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonView;
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
