8c399141f83abf27aff39cfbe66600b614b2d37b
emerson <emerson@unovation.com.br>
2017-07-05 23:11:47 -0300

    remove: test

--- src/test/groovy/br/com/unopay/api/service/BatchClosingServiceTest.groovy
+++ src/test/groovy/br/com/unopay/api/service/BatchClosingServiceTest.groovy
@@ -45,33 +45,6 @@ class BatchClosingServiceTest extends SpockApplicationTests {
         service.notificationService = notificationServiceMock
     }
 
-    def 'given a batch processing should not open concurrent process'(){
-        given:
-        List<Contract> contracts = Fixture.from(Contract.class).uses(jpaProcessor).gimme(1, "valid")
-        Establishment establishment = Fixture.from(Establishment.class).uses(jpaProcessor).gimme("valid")
-        createServiceAuthorizations(contracts, establishment, 40)
-
-        ExecutorService executor = Executors.newFixedThreadPool(1)
-        when:
-        List<Callable<String>> callable = Arrays.asList(
-        new Callable() {
-            @Override
-            Object call() throws Exception {
-                return  service.create(establishment.id, instant("1 day ago"))
-            }
-        },
-        new Callable() {
-            @Override
-            Object call() throws Exception {
-                return  service.create(establishment.id, instant("1 day ago"))
-            }
-        })
-        executor.invokeAll(callable).each { it.get() }
-
-        then:
-        def ex = thrown(UnprocessableEntityException)
-        assert ex.errors.first().logref == 'BATCH_ALREADY_RUNNING'
-    }
 
     def 'should ever create new batch closing when processed'(){
         given:
