e965a7ee7726cd1ccbf7c69d4780ae79fa86a68c
emerson <emerson@unovation.com.br>
2017-08-01 19:40:13 -0300

    fix test

--- src/main/java/br/com/unopay/api/payment/repository/PaymentRemittanceRepository.java
+++ src/main/java/br/com/unopay/api/payment/repository/PaymentRemittanceRepository.java
@@ -10,7 +10,7 @@ import java.util.Set;
 
 public interface PaymentRemittanceRepository
                             extends UnovationFilterRepository<PaymentRemittance,String, PaymentRemittanceFilter> {
-    List<PaymentRemittance> findByPayerDocumentNumber(String payerDocument);
+    List<PaymentRemittance> findByPayerDocumentNumberOrderByCreatedDateTime(String payerDocument);
     Optional<PaymentRemittance> findByPayerDocumentNumberAndSituation(String documentNumber,
                                                                       RemittanceSituation situation);
     Optional<PaymentRemittance> findByNumber(String number);
