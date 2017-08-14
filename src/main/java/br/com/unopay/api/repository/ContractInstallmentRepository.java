2cf88eb5e13c13542cf1c6abc2f5ce16a12270de
emerson <emerson@unovation.com.br>
2017-08-14 20:14:27 -0300

    add: create installment when create contract

--- src/main/java/br/com/unopay/api/repository/ContractInstallmentRepository.java
+++ src/main/java/br/com/unopay/api/repository/ContractInstallmentRepository.java
@@ -2,11 +2,13 @@ package br.com.unopay.api.repository;
 
 import br.com.unopay.api.model.ContractInstallment;
 import java.util.Optional;
+import java.util.Set;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ContractInstallmentRepository
         extends CrudRepository<ContractInstallment, String> {
 
     Optional<ContractInstallment> findById(String id);
+    Set<ContractInstallment> findByContractId(String contractId);
 
 }
--- src/test/groovy/br/com/unopay/api/model/ContractInstallmentTest.groovy
+++ src/test/groovy/br/com/unopay/api/model/ContractInstallmentTest.groovy
@@ -2,8 +2,92 @@ package br.com.unopay.api.model
 
 import br.com.six2six.fixturefactory.Fixture
 import br.com.unopay.api.FixtureApplicationTest
+import static br.com.unopay.api.function.FixtureFunctions.instant
+import br.com.unopay.api.util.Rounder
 
 class ContractInstallmentTest extends FixtureApplicationTest {
+
+    def 'when create from contract the value should be the annuity by payment installments'(){
+        given:
+        Contract contract = Fixture.from(Contract.class).gimme("valid")
+
+        when:
+        def installment = new ContractInstallment(contract)
+
+        then:
+        installment.value == Rounder.round(contract.annuity / contract.paymentInstallments)
+    }
+
+    def 'when create from contract the contract should be the contract param'(){
+        given:
+        Contract contract = Fixture.from(Contract.class).gimme("valid")
+
+        when:
+        def installment = new ContractInstallment(contract)
+
+        then:
+        installment.contract.id == contract.id
+    }
+
+    def 'when create from contract the number should be one'(){
+        given:
+        Contract contract = Fixture.from(Contract.class).gimme("valid")
+
+        when:
+        def installment = new ContractInstallment(contract)
+
+        then:
+        installment.installmentNumber == 1
+    }
+
+    def 'when create from contract the expiration should be after one month'(){
+        given:
+        Contract contract = Fixture.from(Contract.class).gimme("valid")
+
+        when:
+        def installment = new ContractInstallment(contract)
+
+        then:
+        installment.expiration > instant("one month from now at 00:01 am")
+        installment.expiration < instant("one month from now at 23:59 pm")
+    }
+
+    def 'when increment expiration should be incremented with one month'(){
+        given:
+        Contract contract = Fixture.from(Contract.class).gimme("valid")
+
+        when:
+        def contractInstallment = new ContractInstallment(contract)
+
+        then:
+        int nextMonth = 1
+        Date expiration = contractInstallment.expiration
+        (0..contract.paymentInstallments).every {
+            def installment = new ContractInstallment(contract)
+            installment.plusExpiration(expiration)
+            nextMonth++
+            expiration = installment.expiration
+            installment.expiration > instant("$nextMonth month from now at 00:01 am") &&
+            installment.expiration < instant("$nextMonth month from now at 23:59 pm")
+        }
+    }
+
+    def 'installment number should be incremented'(){
+        given:
+        Contract contract = Fixture.from(Contract.class).gimme("valid")
+
+        when:
+        def contractInstallment = new ContractInstallment(contract)
+
+        then:
+        int nextNumber = contractInstallment.installmentNumber
+        (0..contract.paymentInstallments).every {
+            def installment = new ContractInstallment(contract)
+            installment.incrementNumber(nextNumber)
+            nextNumber++
+            installment.installmentNumber == nextNumber
+        }
+    }
 
     def 'should be equals'(){
         given:
