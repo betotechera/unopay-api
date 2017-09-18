b3c61e8072625c64b9478cd7cdda4f9b2cf968ea
emerson <emerson@unovation.com.br>
2017-09-18 16:20:15 -0300

    Committing release jar version 0.0.312

--- src/test/groovy/br/com/unopay/api/ProductTemplateLoader.java
+++ src/test/groovy/br/com/unopay/api/ProductTemplateLoader.java
@@ -41,7 +41,7 @@ public class ProductTemplateLoader implements TemplateLoader {
             add("serviceTypes", Arrays.asList(ServiceType.FUEL_ALLOWANCE, ServiceType.FREIGHT_RECEIPT));
             add("creditInsertionTypes", Arrays.asList(CreditInsertionType.values()));
             add("contractValidityDays", 360);
-            add("paymentInstallments", random(Integer.class, range(1,12)));
+            add("paymentInstallments", random(Integer.class, range(2,12)));
             add("annuity", random(BigDecimal.class, range(100, 250)));
         }});
 
