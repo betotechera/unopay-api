f61c6caf9865a9900c2e717a901a725e7393d739
emerson <emerson@unovation.com.br>
2017-11-29 14:17:18 -0200

    add: issuer, network and establishment me endpoints

--- src/test/groovy/br/com/unopay/api/bacen/controller/AccreditedNetworkControllerTest.groovy
+++ src/test/groovy/br/com/unopay/api/bacen/controller/AccreditedNetworkControllerTest.groovy
@@ -2,13 +2,14 @@ package br.com.unopay.api.bacen.controller
 
 import br.com.six2six.fixturefactory.Fixture
 import br.com.unopay.api.bacen.model.AccreditedNetwork
+import br.com.unopay.api.bacen.util.FixtureCreator
 import br.com.unopay.api.uaa.AuthServerApplicationTests
 import static org.hamcrest.Matchers.equalTo
 import static org.hamcrest.Matchers.greaterThan
 import static org.hamcrest.core.Is.is
 import static org.hamcrest.core.IsNull.notNullValue
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
-import org.springframework.test.web.servlet.MvcResult
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
@@ -22,6 +23,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class AccreditedNetworkControllerTest extends AuthServerApplicationTests {
     private static final String ACCREDITED_NETWORK_ENDPOINT = '/accredited-networks?access_token={access_token}'
     private static final String ACCREDITED_NETWORK_ID_ENDPOINT = '/accredited-networks/{id}?access_token={access_token}'
+
+    @Autowired
+    FixtureCreator fixtureCreator
 
     
     void 'should create accreditedNetwork'() {
@@ -40,9 +44,8 @@ class AccreditedNetworkControllerTest extends AuthServerApplicationTests {
     void 'known accreditedNetwork should be deleted'() {
         given:
         String accessToken = getUserAccessToken()
-        def mvcResult = this.mvc.perform(postAccreditedNetwork(accessToken, getAccreditedNetwork())).andReturn()
-        def location = getLocationHeader(mvcResult)
-        def id = extractId(location)
+        def network = fixtureCreator.createNetwork()
+        def id = network.id
         when:
         def result = this.mvc.perform(delete(ACCREDITED_NETWORK_ID_ENDPOINT,id, accessToken).contentType(MediaType.APPLICATION_JSON))
         then:
@@ -52,38 +55,29 @@ class AccreditedNetworkControllerTest extends AuthServerApplicationTests {
     void 'known accreditedNetwork should be updated'() {
         given:
         String accessToken = getUserAccessToken()
-        def mvcResult = this.mvc.perform(postAccreditedNetwork(accessToken, getAccreditedNetwork())).andReturn()
-        def location = getLocationHeader(mvcResult)
-        def id = extractId(location)
+        def network = fixtureCreator.createNetwork()
+        def id = network.id
         when:
         def result = this.mvc.perform(put(ACCREDITED_NETWORK_ID_ENDPOINT,id, accessToken)
-                .content(toJson(accreditedNetwork.with { id= extractId(location);person.name = 'updated';it }))
+                .content(toJson(accreditedNetwork.with { person.name = 'updated';it }))
                 .contentType(MediaType.APPLICATION_JSON))
         then:
         result.andExpect(status().isNoContent())
     }
 
-    String extractId(String location) {
-        location.replaceAll('/accredited-networks/', "")
-    }
-
-    String getLocationHeader(MvcResult mvcResult) {
-        mvcResult.getResponse().getHeader("Location")
-    }
-
     void 'known accreditedNetwork should be found'() {
         given:
-            String accessToken = getUserAccessToken()
-            AccreditedNetwork accreditedNetwork = getAccreditedNetwork()
-            def mvcResult = this.mvc.perform(postAccreditedNetwork(accessToken, accreditedNetwork)).andReturn()
-            def location = getLocationHeader(mvcResult)
-            def id = extractId(location)
+        String accessToken = getUserAccessToken()
+        def network = fixtureCreator.createNetwork()
+        def id = network.id
+
         when:
-            def result = this.mvc.perform(get(ACCREDITED_NETWORK_ID_ENDPOINT,id, accessToken).contentType(MediaType.APPLICATION_JSON))
+        def result = this.mvc.perform(get(ACCREDITED_NETWORK_ID_ENDPOINT,id, accessToken).contentType(MediaType.APPLICATION_JSON))
+
         then:
-            result.andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath('$.person.name', is(equalTo(accreditedNetwork.person.name))))
-                .andExpect(MockMvcResultMatchers.jsonPath('$.person.document.number', is(equalTo(accreditedNetwork.person.document.number))))
+        result.andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath('$.person.name', is(equalTo(network.person.name))))
+                .andExpect(MockMvcResultMatchers.jsonPath('$.person.document.number', is(equalTo(network.person.document.number))))
     }
 
     void 'known accreditedNetwork should be found when find all'() {
