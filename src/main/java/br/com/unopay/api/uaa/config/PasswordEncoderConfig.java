0408086144a3885e699cad37ffbb33907c4bbcfe
emerson <emerson@unovation.com.br>
2017-12-04 15:14:18 -0200

    fix tests

--- src/main/java/br/com/unopay/api/uaa/config/PasswordEncoderConfig.java
+++ src/main/java/br/com/unopay/api/uaa/config/PasswordEncoderConfig.java
@@ -2,14 +2,23 @@ package br.com.unopay.api.uaa.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 @Configuration
 public class PasswordEncoderConfig {
 
     @Bean
+    @Profile("!test")
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Profile("test")
+    public PasswordEncoder passwordEncoderTest() {
+        return new StandardPasswordEncoder();
     }
 }
