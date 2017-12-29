715c866769aafa98f05f5e199b77d8b9f02526db
emerson <emerson@unovation.com.br>
2017-12-29 17:05:24 -0200

    add: issuer network reference tests

--- src/main/java/br/com/unopay/api/bacen/service/AccreditedNetworkIssuerService.java
+++ src/main/java/br/com/unopay/api/bacen/service/AccreditedNetworkIssuerService.java
@@ -1,25 +1,51 @@
 package br.com.unopay.api.bacen.service;
 
 import br.com.unopay.api.bacen.model.AccreditedNetworkIssuer;
+import br.com.unopay.api.bacen.model.filter.AccreditedNetworkIssuerFilter;
 import br.com.unopay.api.bacen.repository.AccreditedNetworkIssuerRepository;
+import br.com.unopay.api.uaa.service.UserDetailService;
+import br.com.unopay.bootcommons.jsoncollections.UnovationPageRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccreditedNetworkIssuerService {
 
     private AccreditedNetworkIssuerRepository repository;
+    private AccreditedNetworkService accreditedNetworkService;
+    private UserDetailService userDetailService;
+    private IssuerService issuerService;
 
     @Autowired
-    public AccreditedNetworkIssuerService(AccreditedNetworkIssuerRepository repository) {
+    public AccreditedNetworkIssuerService(AccreditedNetworkIssuerRepository repository,
+                                          AccreditedNetworkService accreditedNetworkService,
+                                          UserDetailService userDetailService,
+                                          IssuerService issuerService) {
         this.repository = repository;
+        this.accreditedNetworkService = accreditedNetworkService;
+        this.userDetailService = userDetailService;
+        this.issuerService = issuerService;
     }
 
-    public AccreditedNetworkIssuer create(AccreditedNetworkIssuer networkIssuer) {
+    public AccreditedNetworkIssuer create(String currentUserMail, AccreditedNetworkIssuer networkIssuer) {
+        setReferences(currentUserMail, networkIssuer);
         return repository.save(networkIssuer);
+    }
+
+    private void setReferences(String currentUserMail, AccreditedNetworkIssuer networkIssuer) {
+        networkIssuer
+                .setAccreditedNetwork(accreditedNetworkService.getById(networkIssuer.getAccreditedNetwork().getId()));
+        networkIssuer.setIssuer(issuerService.findById(networkIssuer.getIssuer().getId()));
+        networkIssuer.setUser(userDetailService.getByEmail(currentUserMail));
     }
 
     public AccreditedNetworkIssuer findById(String id) {
         return repository.findOne(id);
+    }
+
+    public Page<AccreditedNetworkIssuer> findByFilter(AccreditedNetworkIssuerFilter filter, UnovationPageRequest pageable) {
+        return repository.findAll(filter, new PageRequest(pageable.getPageStartingAtZero(), pageable.getSize()));
     }
 }
--- src/test/groovy/br/com/unopay/api/bacen/service/AccreditedNetworkIssuerServiceTest.groovy
+++ src/test/groovy/br/com/unopay/api/bacen/service/AccreditedNetworkIssuerServiceTest.groovy
@@ -5,6 +5,7 @@ import br.com.six2six.fixturefactory.Rule
 import br.com.unopay.api.SpockApplicationTests
 import br.com.unopay.api.bacen.model.AccreditedNetworkIssuer
 import br.com.unopay.api.bacen.util.FixtureCreator
+import br.com.unopay.bootcommons.exception.NotFoundException
 import org.springframework.beans.factory.annotation.Autowired
 
 class AccreditedNetworkIssuerServiceTest extends SpockApplicationTests {
@@ -13,6 +14,10 @@ class AccreditedNetworkIssuerServiceTest extends SpockApplicationTests {
     private AccreditedNetworkIssuerService service
     @Autowired
     private FixtureCreator fixtureCreator
+    private String userEmailUnderTest
+    void setup(){
+        userEmailUnderTest = fixtureCreator.createUser().email
+    }
 
     def 'should create AccreditedNetworkIssuerService'(){
         given:
@@ -23,10 +28,55 @@ class AccreditedNetworkIssuerServiceTest extends SpockApplicationTests {
         }})
 
         when:
-        AccreditedNetworkIssuer created = service.create(networkIssuer)
+        AccreditedNetworkIssuer created = service.create(userEmailUnderTest, networkIssuer)
         AccreditedNetworkIssuer result = service.findById(created.id)
 
         then:
         result
+    }
+
+    def 'given a unknown issuer when create should return error'(){
+        given:
+        AccreditedNetworkIssuer networkIssuer = Fixture.from(AccreditedNetworkIssuer).gimme("valid", new Rule(){{
+            add("accreditedNetwork", fixtureCreator.createNetwork())
+            add("user", fixtureCreator.createUser())
+        }})
+
+        when:
+        service.create(userEmailUnderTest, networkIssuer)
+
+        then:
+        def ex = thrown(NotFoundException)
+        ex.errors.first().logref == 'ISSUER_NOT_FOUND'
+    }
+
+    def 'given a unknown network when create should return error'(){
+        given:
+        AccreditedNetworkIssuer networkIssuer = Fixture.from(AccreditedNetworkIssuer).gimme("valid", new Rule(){{
+            add("issuer", fixtureCreator.createIssuer())
+            add("user", fixtureCreator.createUser())
+        }})
+
+        when:
+        service.create(userEmailUnderTest, networkIssuer)
+
+        then:
+        def ex = thrown(NotFoundException)
+        ex.errors.first().logref == 'ACCREDITED_NETWORK_NOT_FOUND'
+    }
+
+    def 'given a unknown user when create should return error'(){
+        given:
+        AccreditedNetworkIssuer networkIssuer = Fixture.from(AccreditedNetworkIssuer).gimme("valid", new Rule(){{
+            add("issuer", fixtureCreator.createIssuer())
+            add("accreditedNetwork", fixtureCreator.createNetwork())
+        }})
+
+        when:
+        service.create('', networkIssuer)
+
+        then:
+        def ex = thrown(NotFoundException)
+        ex.errors.first().logref == 'USER_NOT_FOUND'
     }
 }
