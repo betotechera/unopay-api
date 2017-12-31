ff2e45a07fb68a1b5d9b20d77d1ac51d419e23ec
emerson <emerson@unovation.com.br>
2017-12-30 23:58:05 -0200

    fix script

--- src/main/resources/dev/db/migration/hsqldb/V7_6__IssuerOnNetwork.sql
+++ src/main/resources/dev/db/migration/hsqldb/V7_6__IssuerOnNetwork.sql
@@ -4,7 +4,7 @@ create table accredited_network_issuer(
     ISSUER_ID varchar(256) not null,
     CREATED_DATE_TIME timestamp not null,
     USER_ID varchar(256) not null,
-    active integer not null,
+    active varchar(1) not null,
     version integer,
     constraint fk_netiss_network foreign key(ACCREDITED_NETWORK_ID) references accredited_network(id),
     constraint fk_netiss_issuer foreign key(ISSUER_ID) references issuer(id)
