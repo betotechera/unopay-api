0e42adf75a5d1a59aefbeeb7cc1c303a4c582d00
emerson <emerson@unovation.com.br>
2018-01-04 15:29:45 -0200

    remove stack when get address failed

--- src/main/java/br/com/unopay/api/service/AddressService.java
+++ src/main/java/br/com/unopay/api/service/AddressService.java
@@ -24,7 +24,7 @@ public class AddressService {
             AddressSearch addressSearch = addressSearchService.search(zipCode);
             return  new Address(addressSearch);
         }catch (HttpClientErrorException e){
-            log.warn("Error on getting address",e);
+            log.warn("Getting address failed",e.getMessage());
             return new Address(zipCode);
         }
     }
