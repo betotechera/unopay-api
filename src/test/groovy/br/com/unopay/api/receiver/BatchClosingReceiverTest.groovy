81566e8834ea07f18e173fceb6f036f3696e7a6b
Pauloalgelin <pauloalgelin@gmail.com>
2018-01-16 16:25:57 -0200

    add 'BatchClosingReceiverTest'

---
+++ src/test/groovy/br/com/unopay/api/receiver/BatchClosingReceiverTest.groovy
@@ -0,0 +1,35 @@
+package br.com.unopay.api.receiver
+
+import br.com.unopay.api.SpockApplicationTests
+import br.com.unopay.api.bacen.util.FixtureCreator
+import br.com.unopay.api.model.BatchClosing
+import br.com.unopay.api.service.BatchClosingService
+import br.com.unopay.api.util.GenericObjectMapper
+import br.com.six2six.fixturefactory.Fixture
+import br.com.unopay.api.SpockApplicationTests
+import br.com.unopay.api.model.BatchClosing
+import br.com.unopay.api.service.BatchClosingService
+import br.com.unopay.api.util.GenericObjectMapper
+import org.springframework.beans.factory.annotation.Autowired
+
+class BatchClosingReceiverTest extends SpockApplicationTests {
+
+    private BatchClosingReceiver batchClosingReceiver
+    private BatchClosingService batchClosingService = Mock(BatchClosingService)
+    @Autowired
+    private GenericObjectMapper genericObjectMapper
+
+    @Override
+    void setup() {
+        batchClosingReceiver = new BatchClosingReceiver(genericObjectMapper, batchClosingService)
+    }
+
+    def 'batchReceiptNotify'(){
+        given:
+        def batch = Fixture.from(BatchClosing).gimme("valid")
+        when:
+        batchClosingReceiver.batchReceiptNotify(toJson(batch))
+        then:
+        1 * batchClosingService.process(_,_)
+    }
+}
