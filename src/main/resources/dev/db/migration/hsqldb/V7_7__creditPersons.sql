a8deeff1288abba3ec7580e3f3a9dc0b8367a277
emerson <emerson@unovation.com.br>
2018-01-27 17:44:45 -0200

    fix script for tests

--- src/main/resources/dev/db/migration/hsqldb/V7_7__creditPersons.sql
+++ src/main/resources/dev/db/migration/hsqldb/V7_7__creditPersons.sql
@@ -2,4 +2,4 @@ alter table credit add column issuer_id varchar(256) not null;
 alter table credit add column hirer_id varchar(256) not null;
 alter table credit drop column issuer_document;
 alter table credit drop column hirer_document;
-alter table hirer add financier_mail varchar(100) not null;
+alter table hirer add column financier_mail varchar(100) null;
