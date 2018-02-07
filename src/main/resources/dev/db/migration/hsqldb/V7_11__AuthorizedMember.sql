4d45f187918498104e52bb4eb40a8c0480fbbf9c
Renan Silva <renan.tssilva@gmail.com>
2018-02-07 16:55:22 -0200

    add MemberAuthorized sql script

---
+++ src/main/resources/dev/db/migration/hsqldb/V7_11__AuthorizedMember.sql
@@ -0,0 +1,13 @@
+create table authorized_member(
+    id VARCHAR(256) PRIMARY KEY,
+    birth_date TIMESTAMP not null,
+    name varchar(255) not null,
+    gender varchar(50) not null,
+    relatedness varchar(50) not null,
+    email VARCHAR(256),
+    document_type VARCHAR(50),
+    document_number VARCHAR(50),
+    registry_entity VARCHAR(50),
+    payment_instrument_id VARCHAR(256)
+    constraint fk_auth_member_pay_inst foreign key(hirer_id) references hirer(id)
+);
