1c8ff3415936de4311b6f9966aac99e49efdd964
Pauloalgelin <pauloalgelin@gmail.com>
2018-02-09 17:36:38 -0200

    add getByParams to UserCreditCardController and its test to UserCreditCardControllerTest

--- src/main/java/br/com/unopay/api/billing/creditcard/controller/UserCreditCardController.java
+++ src/main/java/br/com/unopay/api/billing/creditcard/controller/UserCreditCardController.java
@@ -1,4 +1,50 @@
 package br.com.unopay.api.billing.creditcard.controller;
 
+import br.com.unopay.api.billing.creditcard.model.UserCreditCard;
+import br.com.unopay.api.billing.creditcard.model.filter.UserCreditCardFilter;
+import br.com.unopay.api.billing.creditcard.service.UserCreditCardService;
+import br.com.unopay.api.model.validation.group.Views;
+import br.com.unopay.bootcommons.jsoncollections.PageableResults;
+import br.com.unopay.bootcommons.jsoncollections.Results;
+import br.com.unopay.bootcommons.jsoncollections.UnovationPageRequest;
+import br.com.unopay.bootcommons.stopwatch.annotation.Timed;
+import com.fasterxml.jackson.annotation.JsonView;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@Timed(prefix = "api")
 public class UserCreditCardController {
+
+    @Value("${unopay.api}")
+    private String api;
+
+    private UserCreditCardService userCreditCardService;
+
+    @Autowired
+    public UserCreditCardController(UserCreditCardService userCreditCardService) {
+        this.userCreditCardService = userCreditCardService;
+    }
+
+    @JsonView(Views.UserCreditCard.List.class)
+    @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasRole('ROLE_LIST_USER_CREDIT_CARD')")
+    @RequestMapping(value = "/credit-cards", method = RequestMethod.GET)
+    public Results<UserCreditCard> getByParams(UserCreditCardFilter userCreditCardFilter,
+                                               @Validated UnovationPageRequest pageable) {
+        log.info("search user credit card with filter={}", userCreditCardFilter);
+        Page<UserCreditCard> page = userCreditCardService.findByFilter(userCreditCardFilter, pageable);
+        pageable.setTotal(page.getTotalElements());
+        return PageableResults.create(pageable, page.getContent(), String.format("%s/credit-cards", api));
+    }
 }
---
+++ src/test/groovy/br/com/unopay/api/billing/creditcard/controller/UserCreditCardControllerTest.groovy
@@ -0,0 +1,53 @@
+package br.com.unopay.api.billing.creditcard.controller
+
+import br.com.six2six.fixturefactory.Fixture
+import br.com.six2six.fixturefactory.Rule
+import br.com.unopay.api.bacen.util.FixtureCreator
+import br.com.unopay.api.billing.creditcard.model.UserCreditCard
+import br.com.unopay.api.billing.creditcard.service.UserCreditCardService
+import br.com.unopay.api.uaa.AuthServerApplicationTests
+import br.com.unopay.api.uaa.model.UserDetail
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+
+import static org.hamcrest.Matchers.equalTo
+import static org.hamcrest.Matchers.notNullValue
+import static org.hamcrest.core.Is.is
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+class UserCreditCardControllerTest extends AuthServerApplicationTests {
+
+    @Autowired
+    private UserCreditCardService userCreditCardService
+
+    @Autowired
+    private FixtureCreator fixtureCreator
+
+    private UserDetail userDetail
+
+    void setup(){
+        userDetail = fixtureCreator.createUser()
+    }
+
+    void 'all user credit cards should be found'(){
+
+        given:
+        UserCreditCard userCreditCard = Fixture.from(UserCreditCard).gimme("valid", new Rule(){{
+            add("user", userDetail)
+        }})
+        userCreditCardService.create(userCreditCard)
+        String accessToken = getUserAccessToken()
+
+        when:
+        def result = this.mvc.perform(get('/credit-cards?access_token={access_token}', accessToken)
+                .contentType(MediaType.APPLICATION_JSON))
+
+        then:
+        result.andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath('$.total', is(equalTo(1))))
+                .andExpect(MockMvcResultMatchers.jsonPath('$.items[0].id', is(notNullValue())))
+
+    }
+}
