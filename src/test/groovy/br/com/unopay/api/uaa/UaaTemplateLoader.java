9893a82946d44186f40b95d19d6e122f6c22e585
emerson <emerson@unovation.com.br>
2018-03-16 16:54:54 -0300

    identity

--- src/test/groovy/br/com/unopay/api/uaa/UaaTemplateLoader.java
+++ src/test/groovy/br/com/unopay/api/uaa/UaaTemplateLoader.java
@@ -15,7 +15,7 @@ public class UaaTemplateLoader implements TemplateLoader {
     @Override
     public void load() {
 
-        Fixture.of(UserDetail.class).addTemplate("without-group", new Rule(){{
+        Fixture.of(UserDetail.class).addTemplate("without-group", new Rule() {{
             add("id", regex("\\w{20}"));
             add("email", "${name}@gmail.com");
             add("name", regex("\\d{2}\\w{8}"));
@@ -23,31 +23,31 @@ public class UaaTemplateLoader implements TemplateLoader {
             add("password", regex("\\d{5,8}"));
         }});
 
-        Fixture.of(UserDetail.class).addTemplate("without-institution").inherits("without-group", new Rule(){{
+        Fixture.of(UserDetail.class).addTemplate("without-institution").inherits("without-group", new Rule() {{
             add("type", one(UserType.class, "institution"));
         }});
 
-        Fixture.of(UserDetail.class).addTemplate("with-institution").inherits("without-group", new Rule(){{
+        Fixture.of(UserDetail.class).addTemplate("with-institution").inherits("without-group", new Rule() {{
             add("institution", one(Institution.class, "user"));
         }});
 
-        Fixture.of(UserDetail.class).addTemplate("with-group").inherits("without-group", new Rule(){{
+        Fixture.of(UserDetail.class).addTemplate("with-group").inherits("without-group", new Rule() {{
             add("groups", has(1).of(Group.class, "valid"));
         }});
 
-        Fixture.of(UserDetail.class).addTemplate("group-with-unknown-role").inherits("without-group", new Rule(){{
+        Fixture.of(UserDetail.class).addTemplate("group-with-unknown-role").inherits("without-group", new Rule() {{
             add("groups", has(1).of(Group.class, "with-unknown-role"));
         }});
 
 
-        Fixture.of(Group.class).addTemplate("valid", new Rule(){{
+        Fixture.of(Group.class).addTemplate("valid", new Rule() {{
             add("name", uniqueRandom("adm", "atndente"));
             add("description", random("grupo 1", "grupo 2"));
             add("userType", one(UserType.class, "valid"));
             add("authorities", has(1).of(Authority.class, "valid-admin"));
         }});
 
-        Fixture.of(Group.class).addTemplate("with-id", new Rule(){{
+        Fixture.of(Group.class).addTemplate("with-id", new Rule() {{
             add("id", regex("\\w{20}"));
             add("name", uniqueRandom("adm", "atndente"));
             add("description", random("grupo 1", "grupo 2"));
@@ -56,42 +56,42 @@ public class UaaTemplateLoader implements TemplateLoader {
         }});
 
 
-        Fixture.of(Group.class).addTemplate("with-unknown-role", new Rule(){{
+        Fixture.of(Group.class).addTemplate("with-unknown-role", new Rule() {{
             add("name", uniqueRandom("adm", "atndente"));
             add("description", random("grupo 1", "grupo 2"));
             add("userType", one(UserType.class, "valid"));
             add("authorities", has(1).of(Authority.class, "unknown"));
         }});
 
-        Fixture.of(Group.class).addTemplate("without-name", new Rule(){{
+        Fixture.of(Group.class).addTemplate("without-name", new Rule() {{
             add("description", random("grupo 1", "grupo 2"));
             add("userType", one(UserType.class, "valid"));
             add("authorities", Arrays.asList("ROLE_ADMIN", "ROLE_UNKNOWN"));
         }});
 
-        Fixture.of(Authority.class).addTemplate("valid", new Rule(){{
+        Fixture.of(Authority.class).addTemplate("valid", new Rule() {{
             add("name", uniqueRandom("ROLE_ADMIN", "ROLE_USER"));
             add("description", uniqueRandom("Role 1", "Role 2"));
         }});
 
-        Fixture.of(Authority.class).addTemplate("valid-admin", new Rule(){{
+        Fixture.of(Authority.class).addTemplate("valid-admin", new Rule() {{
             add("name", uniqueRandom("ROLE_ADMIN"));
             add("description", uniqueRandom("Role 1", "Role 2"));
         }});
 
-        Fixture.of(Authority.class).addTemplate("unknown", new Rule(){{
+        Fixture.of(Authority.class).addTemplate("unknown", new Rule() {{
             add("name", uniqueRandom("ROLE_UNKNOWN_1", "ROLE_UNKNOWN_2"));
             add("description", uniqueRandom("Role 1", "Role 2"));
         }});
 
-        Fixture.of(UserType.class).addTemplate("valid", new Rule(){{
+        Fixture.of(UserType.class).addTemplate("valid", new Rule() {{
             add("id", "3");
             add("name", uniqueRandom("MASTER", "SUPER"));
             add("description", uniqueRandom("User type 1", "User type 2"));
         }});
 
 
-        Fixture.of(UserType.class).addTemplate("institution", new Rule(){{
+        Fixture.of(UserType.class).addTemplate("institution", new Rule() {{
             add("id", "2");
             add("name", "INSTITUIDOR");
             add("description", "Instituidor de arranjo de Pagamento");
