822a9a9a9b9a758dd6881b7ca6055c94ecbd119b
Pauloalgelin <pauloalgelin@gmail.com>
2018-04-09 09:48:10 -0300

    add issuerDocument, hirerDocument, status, createdDateTime and installmentNumber as filters Negotiation Billing

--- src/main/java/br/com/unopay/api/market/model/filter/NegotiationBillingFilter.java
+++ src/main/java/br/com/unopay/api/market/model/filter/NegotiationBillingFilter.java
@@ -1,5 +1,7 @@
 package br.com.unopay.api.market.model.filter;
 
+import br.com.unopay.api.order.model.PaymentStatus;
+import br.com.unopay.bootcommons.model.Period;
 import br.com.unopay.bootcommons.repository.filter.SearchableField;
 import lombok.Data;
 
@@ -11,4 +13,19 @@ public class NegotiationBillingFilter {
 
     @SearchableField(field = "hirerNegotiation.hirer.id")
     private String hirer;
+
+    @SearchableField(field = "hirerNegotiation.product.issuer.person.document.number")
+    private String issuerDocument;
+
+    @SearchableField(field = "hirerNegotiation.hirer.person.document.number")
+    private String hirerDocument;
+
+    @SearchableField
+    private PaymentStatus status;
+
+    @SearchableField
+    private Period createdDateTime;
+
+    @SearchableField
+    private Integer installmentNumber;
 }
