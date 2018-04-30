febb70d2ae00f3ca99abe54bfa48a6ae99cae619
Pauloalgelin <pauloalgelin@gmail.com>
2018-04-30 14:44:32 -0300

    add Contractor Bonus Filter

---
+++ src/main/java/br/com/unopay/api/market/model/filter/ContractorBonusFilter.java
@@ -0,0 +1,12 @@
+package br.com.unopay.api.market.model.filter;
+
+import lombok.Data;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Data
+@ToString
+public class ContractorBonusFilter implements Serializable {
+    public ContractorBonusFilter(){}
+}
