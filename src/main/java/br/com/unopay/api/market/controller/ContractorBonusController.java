a9c4f91d9266080b854e86ec36f6cdd771748a2b
Pauloalgelin <pauloalgelin@gmail.com>
2018-05-06 14:51:02 -0300

    add Contractor Bonus Controller and getAll and its test

---
+++ src/main/java/br/com/unopay/api/market/controller/ContractorBonusController.java
@@ -0,0 +1,50 @@
+package br.com.unopay.api.market.controller;
+
+import br.com.unopay.api.market.model.ContractorBonus;
+import br.com.unopay.api.market.model.filter.ContractorBonusFilter;
+import br.com.unopay.api.market.service.ContractorBonusService;
+import br.com.unopay.api.model.validation.group.Views;
+import br.com.unopay.bootcommons.jsoncollections.PageableResults;
+import br.com.unopay.bootcommons.jsoncollections.Results;
+import br.com.unopay.bootcommons.jsoncollections.UnovationPageRequest;
+import br.com.unopay.bootcommons.stopwatch.annotation.Timed;
+import com.fasterxml.jackson.annotation.JsonView;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@Timed(prefix = "api")
+public class ContractorBonusController {
+
+    @Value("${unopay.api}")
+    private String api;
+
+    private ContractorBonusService contractorBonusService;
+
+    @Autowired
+    public ContractorBonusController(ContractorBonusService contractorBonusService) {
+        this.contractorBonusService = contractorBonusService;
+    }
+
+    @JsonView(Views.ContractorBonus.List.class)
+    @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasRole('ROLE_LIST_CONTRACTOR_BONUS')")
+    @RequestMapping(value = "/contractor-bonuses", method = RequestMethod.GET)
+    public Results<ContractorBonus> getByParams(ContractorBonusFilter contractorBonusFilter,
+                                                      @Validated UnovationPageRequest pageable) {
+        log.info("search contractor bonus with filter={}", contractorBonusFilter);
+        Page<ContractorBonus> page = contractorBonusService.findByFilter(contractorBonusFilter, pageable);
+        pageable.setTotal(page.getTotalElements());
+        return PageableResults.create(pageable, page.getContent(), String.format("%s/contractor-bonuses", api));
+    }
+}
---
+++ src/test/groovy/br/com/unopay/api/market/controller/ContractorBonusControllerTest.groovy
@@ -0,0 +1,70 @@
+package br.com.unopay.api.market.controller
+
+import br.com.six2six.fixturefactory.Fixture
+import br.com.unopay.api.bacen.model.Contractor
+import br.com.unopay.api.bacen.util.FixtureCreator
+import br.com.unopay.api.market.model.ContractorBonus
+import br.com.unopay.api.market.service.ContractorBonusService
+import br.com.unopay.api.model.Person
+import br.com.unopay.api.model.Product
+import br.com.unopay.api.uaa.AuthServerApplicationTests
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+
+import static org.hamcrest.Matchers.equalTo
+import static org.hamcrest.Matchers.notNullValue
+import static org.hamcrest.core.Is.is
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+class ContractorBonusControllerTest extends AuthServerApplicationTests {
+
+
+    @Autowired
+    private ContractorBonusService contractorBonusService;
+
+    @Autowired
+    private FixtureCreator fixtureCreator
+
+    private Contractor contractorUnderTest
+    private Person personUnderTest
+    private Product productUnderTest
+
+    void setup() {
+        productUnderTest
+        contractorUnderTest = fixtureCreator.createContractor()
+        personUnderTest = contractorUnderTest.person
+        productUnderTest = fixtureCreator.createProduct()
+    }
+
+    void 'all Contractor Bonus should be found'() {
+
+        given:
+        ContractorBonus contractorBonus = createContractorBonus()
+        contractorBonusService.save(contractorBonus)
+        String accessToken = getUserAccessToken()
+
+        when:
+        def result = this.mvc.perform(get('/contractor-bonuses?access_token={access_token}', accessToken)
+                .contentType(MediaType.APPLICATION_JSON))
+
+        then:
+        result.andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath('$.total', is(equalTo(1))))
+                .andExpect(MockMvcResultMatchers.jsonPath('$.items[0].id', is(notNullValue())))
+
+    }
+
+
+    private ContractorBonus createContractorBonus() {
+        ContractorBonus contractorBonus = Fixture.from(ContractorBonus.class).gimme("valid")
+        contractorBonus = contractorBonus.with {
+            product = productUnderTest
+            contractor = contractorUnderTest
+            payer = personUnderTest
+            it
+        }
+        contractorBonus
+    }
+}
