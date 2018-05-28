df02ae8c9218c99ab3f1d1f2079fcbf93b0fe8ef
Pauloalgelin <pauloalgelin@gmail.com>
2018-05-28 11:44:00 -0300

    alter Contractor Bonus Change script

--- src/main/resources/dev/db/migration/hsqldb/V10_2__ContractorBonusChange.sql
+++ src/main/resources/dev/db/migration/hsqldb/V10_2__ContractorBonusChange.sql
@@ -1,2 +1,2 @@
-alter table contractor_bonus add service_value decimal(20,2);
+alter table contractor_bonus add service_value decimal(20,2) not null;
 alter table contractor_bonus add service_identification VARCHAR(256);
