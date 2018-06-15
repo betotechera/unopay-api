257baad0a98bc04b954a045716defdb801b09044
emerson <emerson@unovation.com.br>
2018-06-15 17:00:07 -0300

    add fix ruby script

---
+++ script-fix-installments.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+require 'pg'
+require 'date'
+
+conn = PG::Connection.new("dbname=unopay port=5432 host=unopay-qa.c2u10rjpdtxz.us-west-2.rds.amazonaws.com user=administrator password=LVTQB5MkBuH0" )
+
+
+res = conn.exec_params("""SELECT ci.installment_number, ci.expiration, c.begin_date, p.name, c.code, * from unovation.contract_installment ci 
+    inner join unovation.contract c on ci.contract_id = c.id 
+    inner join unovation.product p on c.product_id = p.id
+    inner join unovation.issuer i on i.id = p.issuer_id
+    inner join unovation.hirer h on h.person_id = i.person_id
+    where h.id = c.hirer_id and i.id = p.issuer_id""")
+
+res.each { |installment|
+   expiration = DateTime.parse(installment['expiration'])
+   installment_number = installment['installment_number']
+   newExpiration = expiration << 1  
+   puts '-----------------------------------------------------------------' if installment_number.eql? '1' 
+   puts "#{installment_number} #{expiration.strftime("%d/%m/%Y")} >> #{newExpiration.strftime("%d/%m/%Y")} #{installment['name']} #{installment['code']}"
+}
+
+conn.finish
