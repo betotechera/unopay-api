a7f0c34cd559d11f634ea9744bdc978da14e6359
emerson <emerson@unovation.com.br>
2018-07-12 01:31:48 -0300

    remove duplicated mark installment as paid

--- src/main/java/br/com/unopay/api/order/service/OrderProcessor.java
+++ src/main/java/br/com/unopay/api/order/service/OrderProcessor.java
@@ -81,9 +81,6 @@ public class OrderProcessor {
         Set<AuthorizedMemberCandidate> candidates = authorizedMemberCandidateService.findByOrderId(order.getId());
         Deal deal = new Deal(order.getPerson(), order.getProductCode(), candidates);
         dealService.closeWithIssuerAsHirer(deal);
-        if(!order.productWithMembershipFee()){
-            contractService.markInstallmentAsPaidFrom(order);
-        }
         log.info("adhesion paid for order={} type={} of value={}",
                 order.getId(),order.getType(), order.getValue());
         notificationService.sendPaymentEmail(order,  EventType.PAYMENT_APPROVED);
