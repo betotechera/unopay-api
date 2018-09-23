8d1f69024f74b6058c6624fc702f2829aa131127
Diogo Techera <diogo@unovation.com.br>
2018-09-23 12:42:18 -0300

    execute all days

--- src/main/java/br/com/unopay/api/job/ContractInstallmentOrderJob.java
+++ src/main/java/br/com/unopay/api/job/ContractInstallmentOrderJob.java
@@ -23,7 +23,7 @@ public class ContractInstallmentOrderJob {
         this.orderService = orderService;
     }
 
-    @Scheduled(cron = "0 0 0 ? * MON-FRI", zone = "GMT-3")
+    @Scheduled(cron = "0 0 0 ? * *", zone = "GMT-3")
     void execute() {
         log.info("running contract installment order job");
         Set<ContractInstallment> installments = contractInstallmentService.findInstallmentAboutToExpire();
