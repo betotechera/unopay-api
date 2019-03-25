47b5d3d03c432849afbaa35b4214b448e5dc50d5
emerson <emerson@unovation.com.br>
2019-03-25 10:43:53 -0300

    add status and type to wingoo user mapping

--- src/main/java/br/com/unopay/api/wingoo/model/WingooUserMapping.java
+++ src/main/java/br/com/unopay/api/wingoo/model/WingooUserMapping.java
@@ -2,13 +2,15 @@ package br.com.unopay.api.wingoo.model;
 
 import br.com.unopay.api.bacen.model.Contractor;
 import br.com.wingoo.userclient.model.User;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import java.io.Serializable;
-import java.util.Date;
 import lombok.Data;
 
 @Data
 public class WingooUserMapping implements Serializable{
+
+    public static final String TYPE = "Aluno";
+    public static final int STATUS = 2;
+    public static final String FORMAT = "%s-%s";
 
     public static User fromContractor(Contractor contractor, String instrumentNumber){
         User user = new User();
@@ -20,8 +22,10 @@ public class WingooUserMapping implements Serializable{
         user.setCellphone(contractor.getPerson().getCellPhone());
         user.setGender(contractor.getPerson().getPhysicalPersonDetail().getGender().getDescription());
         String originalZipCode = contractor.getPerson().getAddress().getZipCode();
-        user.setZipCode(originalZipCode.substring(0, 5) + "-" + originalZipCode.substring(5));
+        user.setZipCode(String.format(FORMAT, originalZipCode.substring(0, 5), originalZipCode.substring(5)));
         user.setStudentId(instrumentNumber);
+        user.setStatus(STATUS);
+        user.setType(TYPE);
         return user;
     }
 }
