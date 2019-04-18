81829b55119638ce5cf5507039294d69199fd096
Weliff Lima <weliff.14@gmail.com>
2019-04-18 01:47:45 -0300

    Adding field to filter

--- src/main/scala/br/com/unopay/api/scheduling/model/filter/SchedulingFilter.scala
+++ src/main/scala/br/com/unopay/api/scheduling/model/filter/SchedulingFilter.scala
@@ -1,6 +1,10 @@
 package br.com.unopay.api.scheduling.model.filter
 
+import java.util.Date
+
+import br.com.unopay.bootcommons.repository.filter.SearchableField
 import lombok.Data
+import org.springframework.core.annotation.AliasFor
 
 import scala.beans.BeanProperty
 
@@ -10,4 +14,14 @@ class SchedulingFilter {
     @BeanProperty
     var token: String = _
 
+    var createdDateTime: Date = _
+
+    @SearchableField(field = "branch.name")
+    var branch: String = _
+
+    @SearchableField(field = "contract.code")
+    var contract: Long = _
+
+    @SearchableField(field = "contractor.person.document.number")
+    var contractorDocument: String = _
 }
