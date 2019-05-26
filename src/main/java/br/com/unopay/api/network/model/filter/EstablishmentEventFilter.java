8bbd09e553b62b6aa24aac0bbf49b500370fd2fd
emerson <emerson@unovation.com.br>
2019-05-26 00:59:54 -0300

    the establishment event filter was fixed

--- src/main/java/br/com/unopay/api/network/model/filter/EstablishmentEventFilter.java
+++ src/main/java/br/com/unopay/api/network/model/filter/EstablishmentEventFilter.java
@@ -1,10 +1,13 @@
 package br.com.unopay.api.network.model.filter;
 
-import br.com.wingoo.reusable.repository.filter.SearchableField;
+import br.com.unopay.bootcommons.repository.filter.SearchableField;
+import java.io.Serializable;
 import lombok.Data;
 
 @Data
-public class EstablishmentEventFilter {
+public class EstablishmentEventFilter implements Serializable {
+
+    public static final Long serialVersionUID = 1L;
 
     @SearchableField(field = "establishment.network.id")
     private String network;
