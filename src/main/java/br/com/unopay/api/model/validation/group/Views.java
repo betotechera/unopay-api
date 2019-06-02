63485c8907b3a96e11d2df87869a90bcf8fec962
emerson <emerson@unovation.com.br>
2019-06-02 00:19:01 -0300

    the sheduling jsonview was fixed

--- src/main/java/br/com/unopay/api/model/validation/group/Views.java
+++ src/main/java/br/com/unopay/api/model/validation/group/Views.java
@@ -205,7 +205,7 @@ public interface Views {
     }
 
     interface Scheduling {
-        interface Detail extends List {}
+        interface Detail extends List, PaymentInstrument.Detail {}
         interface List {}
     }
 
