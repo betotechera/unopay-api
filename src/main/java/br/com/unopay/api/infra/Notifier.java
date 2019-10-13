548afbb78648da0d0d4b03e729198ae9f70f0fc7
emerson <emerson@unovation.com.br>
2019-10-13 00:25:21 -0300

    the order payment was fixed

--- src/main/java/br/com/unopay/api/infra/Notifier.java
+++ src/main/java/br/com/unopay/api/infra/Notifier.java
@@ -2,6 +2,7 @@ package br.com.unopay.api.infra;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,7 @@ public class Notifier {
 
     private String getNotificationAsString(Object notification) {
         try {
+            objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
             return objectMapper.writeValueAsString(notification);
         } catch (JsonProcessingException e) {
             log.warn("could not convert notification to string.", e);
