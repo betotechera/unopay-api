01651cc735aa66b1ad6afec07ade9471d8228477
emerson <emerson@unovation.com.br>
2020-07-24 19:16:45 -0300

    the rettemplate config was changed to try to fix the date serialization

--- src/main/java/br/com/unopay/api/config/WebConfig.java
+++ src/main/java/br/com/unopay/api/config/WebConfig.java
@@ -2,8 +2,11 @@ package br.com.unopay.api.config;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -20,6 +23,11 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(userTypeArgumentResolver);
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
 
 
